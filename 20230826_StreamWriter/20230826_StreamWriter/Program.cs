76ccdfea4fc563cacb4afae74bcdcd3e1915ccf4
sude <sudefidan@icloud.com>
2024-10-13 22:08:02 +0300

    stream-writer

---
+++ 20230826_StreamWriter/20230826_StreamWriter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _20230826_StreamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamWriter sınıfı aracılığı ile bir dosya içerisine yazılar yazdırabiliriz
+            //Bu class ı kullanabilmek için SYstem.IO kütüphanesini eklemek gerekir
+            StreamWriter yazici = new StreamWriter(@"C:\Users\pcc\Desktop\Yeni klasör\deneme.txt");
+
+            yazici.WriteLine("Deneme Metini");
+            yazici.Write("Merhaba");
+            yazici.Write(" ");
+            yazici.Write("Nasılsın");
+
+            //Dosya içerisine yazdırma işlemi bittikten sonra yazıcıyı kapatmamız gerekmektedir
+            yazici.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
